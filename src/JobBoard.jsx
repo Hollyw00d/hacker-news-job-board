373d6c162f44bffc55d1aaf6102d34a20347ed69
Matt Jennings <matt@mattjennings.net>
2026-05-18 16:54:38 -0700

    paused 700 ms before loading fetch

--- src/JobBoard.jsx
+++ src/JobBoard.jsx
@@ -21,6 +21,9 @@ export default function JobBoard() {
         setIsLoading(true);
         setError(null);
 
+        // Pause for 700 ms before executing URL fetch below
+        await new Promise((resolve) => setTimeout(resolve, 700));
+
         const resJobStories = await fetch(
           'https://hacker-news.firebaseio.com/v0/jobstories.json'
         );
